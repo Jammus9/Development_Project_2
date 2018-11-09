b777ad8ad9983b99b7be733e506e38a0f0633395
ATIQ <atik2626@gmail.com>
2018-11-09 20:54:39 +0800

    Update analytics.php
    analytics graphs updated

--- analytics.php
+++ analytics.php
@@ -13,11 +13,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-<<<<<<< HEAD:analytics.php
-    <title>Sales Analytics</title>
-=======
     <title>SB Admin - Dashboard</title>
->>>>>>> parent of 7033cd8... Delete Product:index.html
 
     <!-- Bootstrap core CSS-->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -37,11 +33,7 @@
 
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-<<<<<<< HEAD:analytics.php
-       <a class="navbar-brand mr-1" href="dashboard.php">Family Aid Pharmacy Inc</a>
-=======
       <a class="navbar-brand mr-1" href="index.html">Start Bootstrap</a>
->>>>>>> parent of 7033cd8... Delete Product:index.html
 
       <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
         <i class="fas fa-bars"></i>
@@ -77,50 +69,52 @@
           <!-- Breadcrumbs-->
           <ol class="breadcrumb">
             <li class="breadcrumb-item">
-              <a href="dashboard.php">Dashboard</a>
+              <a href="#">Dashboard</a>
             </li>
             <li class="breadcrumb-item active">Charts</li>
           </ol>
 
-          <!-- Area Chart Example-->
-          <div class="card mb-3">
-            <div class="card-header">
-              <i class="fas fa-chart-area"></i>
-              Sales Analytics Table</div>
-            <div class="card-body">
-              <canvas id="myAreaChart" width="100%" height="30"></canvas>
-            </div>
-            <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-          </div>
-
           <div class="row">
-            <div class="col-lg-8">
+            <div class="col-lg-12">
               <div class="card mb-3">
                 <div class="card-header">
                   <i class="fas fa-chart-bar"></i>
-                  Bar Chart Example</div>
+                  Daily</div>
                 <div class="card-body">
-                  <canvas id="myBarChart" width="100%" height="50"></canvas>
+                  <canvas id="myBarChartDay" width="100%" height="50"></canvas>
                 </div>
-                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-              </div>
-            </div>
-            <div class="col-lg-4">
-              <div class="card mb-3">
-                <div class="card-header">
-                  <i class="fas fa-chart-pie"></i>
-                  Pie Chart Example</div>
-                <div class="card-body">
-                  <canvas id="myPieChart" width="100%" height="100"></canvas>
-                </div>
-                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                <div class="card-footer small text-muted"></div>
               </div>
             </div>
           </div>
-
-          <p class="small text-center text-muted my-5">
-            <em>More chart examples coming soon...</em>
-          </p>
+		  
+		  <div class="row">
+            <div class="col-lg-12">
+              <div class="card mb-3">
+                <div class="card-header">
+                  <i class="fas fa-chart-bar"></i>
+                  Monthly</div>
+                <div class="card-body">
+                  <canvas id="myBarChartMonth" width="100%" height="50"></canvas>
+                </div>
+                <div class="card-footer small text-muted"></div>
+              </div>
+            </div>
+          </div>
+		  
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="card mb-3">
+                <div class="card-header">
+                  <i class="fas fa-chart-bar"></i>
+                  Yearly</div>
+                <div class="card-body">
+                  <canvas id="myBarChartYear" width="100%" height="50"></canvas>
+                </div>
+                <div class="card-footer small text-muted"></div>
+              </div>
+            </div>
+          </div>
 
         </div>
         <!-- /.container-fluid -->
@@ -158,7 +152,7 @@
           <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
           <div class="modal-footer">
             <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="login.html">Logout</a>
+            <a class="btn btn-primary" href="login.php">Logout</a>
           </div>
         </div>
       </div>
@@ -178,9 +172,7 @@
     <script src="js/sb-admin.min.js"></script>
 
     <!-- Demo scripts for this page-->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-bar-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
+    <script src="js/orders_analytics.js"></script>
 
   </body>
 
